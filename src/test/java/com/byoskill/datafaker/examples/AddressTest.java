a77d17c6fd15d2a4edc03803e254e6dbce79de6b
sleroy <contact@sylvainleroy.com>
2018-01-02 22:02:44 +0100

    Extended test

--- src/test/java/com/byoskill/datafaker/examples/AddressTest.java
+++ src/test/java/com/byoskill/datafaker/examples/AddressTest.java
@@ -39,7 +39,6 @@ public class AddressTest extends AbstractFakerUnitTest {
 	@Faker
 	private String cityName;
 
-
 	@Faker("cityName")
 	private String cityName2;
 
@@ -51,6 +50,10 @@ public class AddressTest extends AbstractFakerUnitTest {
 	    return cityName;
 	}
 
+	public String getCityName2() {
+	    return cityName2;
+	}
+
 	public void setCity(final String city) {
 	    this.city = city;
 	}
@@ -59,11 +62,7 @@ public class AddressTest extends AbstractFakerUnitTest {
 	    this.cityName = cityName;
 	}
 
-	public String getCityName2() {
-	    return cityName2;
-	}
-
-	public void setCityName2(String cityName2) {
+	public void setCityName2(final String cityName2) {
 	    this.cityName2 = cityName2;
 	}
 
@@ -109,11 +108,32 @@ public class AddressTest extends AbstractFakerUnitTest {
     @Address()
     public static class AddressExample4 {
 
+	private String buildingNumber;
+
 	private String city;
 
 	private String cityName;
 
+	private String cityPrefix;
+	private String country;
 	private String countryCode;
+	private String firstName;
+	private String lastName;
+	private String latitude;
+	private String longitude;
+	private String secondaryAddress;
+	private String state;
+	private String stateAbbr;
+	private String streetAddress;
+	private String streetAddressNumber;
+	private String streetAddressSecundary;
+	private String streetSuffix;
+	private String timeZone;
+	private String zipCode;
+
+	public String getBuildingNumber() {
+	    return buildingNumber;
+	}
 
 	public String getCity() {
 	    return city;
@@ -123,8 +143,72 @@ public class AddressTest extends AbstractFakerUnitTest {
 	    return cityName;
 	}
 
+	public String getCityPrefix() {
+	    return cityPrefix;
+	}
+
+	public String getCountry() {
+	    return country;
+	}
+
 	public String getCountryCode() {
 	    return countryCode;
+	}
+
+	public String getFirstName() {
+	    return firstName;
+	}
+
+	public String getLastName() {
+	    return lastName;
+	}
+
+	public String getLatitude() {
+	    return latitude;
+	}
+
+	public String getLongitude() {
+	    return longitude;
+	}
+
+	public String getSecondaryAddress() {
+	    return secondaryAddress;
+	}
+
+	public String getState() {
+	    return state;
+	}
+
+	public String getStateAbbr() {
+	    return stateAbbr;
+	}
+
+	public String getStreetAddress() {
+	    return streetAddress;
+	}
+
+	public String getStreetAddressNumber() {
+	    return streetAddressNumber;
+	}
+
+	public String getStreetAddressSecundary() {
+	    return streetAddressSecundary;
+	}
+
+	public String getStreetSuffix() {
+	    return streetSuffix;
+	}
+
+	public String getTimeZone() {
+	    return timeZone;
+	}
+
+	public String getZipCode() {
+	    return zipCode;
+	}
+
+	public void setBuildingNumber(final String buildingNumber) {
+	    this.buildingNumber = buildingNumber;
 	}
 
 	public void setCity(final String city) {
@@ -135,8 +219,68 @@ public class AddressTest extends AbstractFakerUnitTest {
 	    this.cityName = cityName;
 	}
 
+	public void setCityPrefix(final String cityPrefix) {
+	    this.cityPrefix = cityPrefix;
+	}
+
+	public void setCountry(final String country) {
+	    this.country = country;
+	}
+
 	public void setCountryCode(final String countryCode) {
 	    this.countryCode = countryCode;
+	}
+
+	public void setFirstName(final String firstName) {
+	    this.firstName = firstName;
+	}
+
+	public void setLastName(final String lastName) {
+	    this.lastName = lastName;
+	}
+
+	public void setLatitude(final String latitude) {
+	    this.latitude = latitude;
+	}
+
+	public void setLongitude(final String longitude) {
+	    this.longitude = longitude;
+	}
+
+	public void setSecondaryAddress(final String secondaryAddress) {
+	    this.secondaryAddress = secondaryAddress;
+	}
+
+	public void setState(final String state) {
+	    this.state = state;
+	}
+
+	public void setStateAbbr(final String stateAbbr) {
+	    this.stateAbbr = stateAbbr;
+	}
+
+	public void setStreetAddress(final String streetAddress) {
+	    this.streetAddress = streetAddress;
+	}
+
+	public void setStreetAddressNumber(final String streetAddressNumber) {
+	    this.streetAddressNumber = streetAddressNumber;
+	}
+
+	public void setStreetAddressSecundary(final String streetAddressSecundary) {
+	    this.streetAddressSecundary = streetAddressSecundary;
+	}
+
+	public void setStreetSuffix(final String streetSuffix) {
+	    this.streetSuffix = streetSuffix;
+	}
+
+	public void setTimeZone(final String timeZone) {
+	    this.timeZone = timeZone;
+	}
+
+	public void setZipCode(final String zipCode) {
+	    this.zipCode = zipCode;
 	}
 
     }
@@ -164,6 +308,5 @@ public class AddressTest extends AbstractFakerUnitTest {
 	assertNoPropertyNull(PojoFaker.create().randomize(AddressExample4.class));
 
     }
-
 
 }
